3f0a9779de0355b0b35f94fb90d9f0471b68268f
sagartodankar22st-bit <sagartodankar22st-bit@github.com>
2026-05-23 18:09:10 +0530

    Add Chapter 6 operator examples

---
+++ Chapter_06_Operator/30_Operator.js
@@ -0,0 +1,4 @@
+let x=10;
+x=20;
+x=90;
+console.log(x);
---
+++ Chapter_06_Operator/31_Arithmetic_OP.js
@@ -0,0 +1,11 @@
+let a = 10,  b = 3;
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Remainder:", remainder);   
